bfec68338795faca4e3a519f93e624ae0f38206d
John O'Brien <johnobrien.dev@gmail.com>
2024-01-15 20:22:45 -0500

    add update button with local storage

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ let rep = JSON.parse(localStorage.getItem("rep")) || [];
 
 
 const display = document.querySelector(".bottom");
+const topPart = document.querySelector(".top");
+const middlePart = document.querySelector(".middle");
 
 const nameInput = document.querySelector(".standard");
 const composerInput = document.querySelector('.composer');
 const button = document.querySelector('.button');
+
+const updateName = document.querySelector(".update-name");
+const updateComposer = document.querySelector('.update-composer');
+const updateAlert = document.querySelector('.update-alert');
+const updateSubmit = document.querySelector(".update-btn");
 
 ///////FUNCTIONS/////////
 const render = () =>{
@@ -20,23 +27,35 @@ const render = () =>{
 
     rep.forEach((song, index) => {
         const songContainer = document.createElement("div");
-        songContainer.classList.add("container");
         
+
+        const updateButton = document.createElement('button');
+        updateButton.classList.add('btn');
+        updateButton.innerText = "Update";
+        updateButton.addEventListener("click", event =>{
+            topPart.classList.toggle("hide");
+            middlePart.classList.toggle("hide");
+            updateAlert.classList.toggle("hide");
+            updateName.value = song.name;
+            updateComposer.value = song.composer;
+            updateSubmit.setAttribute("updated", index);
+        } )
+
+
         const deleteButton = document.createElement('button');
-        deleteButton.classList.add("delete-btn");
+        deleteButton.classList.add("btn");
         deleteButton.innerText = "Delete";
         deleteButton.addEventListener("click", event =>{
             rep.splice(index,1);
-            
             render();
         })
 
         
-        
-        const newStandard = document.createElement("h3");
+        const newStandard = document.createElement("p");
         newStandard.innerText = `${song.name} by ${song.composer}`;
        
         songContainer.appendChild(newStandard);
+        songContainer.appendChild(updateButton);
         songContainer.appendChild(deleteButton);
        
         display.appendChild(songContainer);
@@ -54,9 +73,23 @@ const createStandard = () =>{
     render();
     nameInput.value = "";
     composerInput.value = "";
-
 }
 
+const updateStandard = event =>{
+    const updatedIndex = event.target.getAttribute("updated");
+    const name = updateName.value;
+    const composer = updateComposer.value;
+    rep[updatedIndex] = { name, composer };
+    render();
+    updateName.value = '';
+    updateComposer.value = '';
+    topPart.classList.toggle("hide");
+    middlePart.classList.toggle("hide");
+    updateAlert.classList.toggle("hide");
+}
+
+
 render();
+updateSubmit.addEventListener("click", updateStandard)
 button.addEventListener("click", createStandard);
 
